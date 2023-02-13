b07e16669306f9c717469162f9e7d836087af9bb
devisceral <develmendes@gmail.com>
2023-02-13 17:00:50 -0300

    fix slideUp

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -41,7 +41,8 @@ const ProjectsSection = () => {
         {projects.map((project, idx) => {
           return (
             <div key={idx}>
-              <SlideUp offset="-300px 0px -300px 0px">
+              {/* <SlideUp offset="-300px 0px -300px 0px"> */}
+              <SlideUp>
                 <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                   <div className="md:w-1/2">
                     <Link href={project.link} target="_blank">
